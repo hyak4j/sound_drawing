f7417a22d8471f8f6d1ba22a68108ac67e798b27
hyak4j <65548056+hyak4j@users.noreply.github.com>
2023-11-23 23:31:53 +0800

    [新增] MainActivity UI

---
+++ app/src/main/java/com/hyak4j/sounddrawing/view/PaintView.kt
@@ -0,0 +1,9 @@
+package com.hyak4j.sounddrawing.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class PaintView(contex: Context, attrs: AttributeSet) : View(contex, attrs) {
+
+}
